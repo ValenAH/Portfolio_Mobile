dd3235119b4df97bc0279314bf11a17f401bf5eb
Ruben Londoño Ramirez <ruben.londo627@gmail.com>
2023-10-08 14:58:23 -0500

    Mi primer commit

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Portfolio from './app/pages/portfolio';
+import Customer from './app/pages/Customer/customer'; 
+import CustomerForm from './app/pages/Customer/customer-form';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator(); 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Portfolio/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name='Portfolio' component={Portfolio}/>
+            <Stack.Screen name='CustomerForm' component= {CustomerForm}/>
+            <Stack.Screen name='Customer' component={Customer}/>
+
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ app/pages/Customer/customer-form.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import {Text, View, TextInput,Button,StyleSheet} from 'react-native';
+import './customer.scss';
+
+const CustomerForm = ({navigation})=>{
+     
+        return (
+            <View>
+                <Text>Registrar Cliente</Text>
+                <Text>Tipo de Documento</Text>
+                <TextInput style={styles.input}/>
+                <Text>Número de Documento</Text>
+                <TextInput style={styles.input}/>
+                <Text>Nombre del Cliente</Text>
+                <TextInput style={styles.input}/>
+                <Text>Teléfono</Text>
+                <TextInput style={styles.input}/>
+                <Text>Correo Electrónico</Text>
+                <TextInput style={styles.input}/>
+                <Text>Dirección</Text>
+                <TextInput style={styles.input}/>
+                <Button 
+                    title="Registrar"
+                    color="#2664bd"
+                    onPress={()=>{navigation.navigate('Portfolio')}}
+                    />
+                
+            </View>
+        );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        height: 25,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+    btn:{
+        height: 25,
+        borderRadius: 10
+    }
+});
+    
+
+export default CustomerForm;
