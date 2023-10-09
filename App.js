77c028718a740399db4697c73e05b75bd9de0443
Ruben Londoño Ramirez <ruben.londo627@gmail.com>
2023-10-08 23:21:33 -0500

    Customer Form Ruben

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Portfolio from './app/pages/portfolio';
-import Customer from './app/pages/Customer/customer'; 
-import CustomerForm from './app/pages/Customer/customer-form';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Portfolio from "./app/pages/portfolio";
+import Customer from "./app/pages/Customer/customer";
+import CustomerForm from "./app/pages/Customer/customer-form";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-const Stack = createNativeStackNavigator(); 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name='Portfolio' component={Portfolio}/>
-            <Stack.Screen name='CustomerForm' component= {CustomerForm}/>
-            <Stack.Screen name='Customer' component={Customer}/>
-
-        </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Portfolio" component={Portfolio} />
+        <Stack.Screen
+          options={{
+            title: "CARTERA",
+            headerStyle: {
+              backgroundColor: "black",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerTitleAlign: "center",
+          }}
+          name="CustomerForm"
+          component={CustomerForm}
+        />
+        <Stack.Screen name="Customer" component={Customer} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
